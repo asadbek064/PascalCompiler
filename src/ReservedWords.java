cb2f82d84487edf4a6dac41de4ea9539e11c1c60
computerfolks <104023807+computerfolks@users.noreply.github.com>
2023-03-10 10:44:51 -0500

    Update ReservedWords.java

--- src/ReservedWords.java
+++ src/ReservedWords.java
@@ -1,9 +1,10 @@
 public class ReservedWords {
     // taken from https://www.tutorialspoint.com/pascal/pascal_basic_syntax.htm
     static final String[] RESERVED_WORDS = { "program", "var", "begin", "end", "if", "then", "else", "while", "do",
-            "repeat", "until", "for", "to", "downto", "write", "writeln", "and", "array", "case", "const", "div", "file",
-            "function", "goto", "in", "label", "mod", "nil", "not", "of", "or", "packed", "procedure", "record", "set", 
+            "repeat", "until", "for", "to", "downto", "write", "writeln", "array", "case", "const", "div", "file",
+            "function", "goto", "in", "label", "mod", "nil", "of", "packed", "procedure", "record", "set", 
             "type", "with", "input", "output", "real", "readline", "writeline", "readln"   };
+    //not, or, and are all reserved words but I left them out for now because they are logical operators, not sure how we are handling that yet
 
     public static boolean isReservedWord(String s) {
         for (String word: RESERVED_WORDS) {
