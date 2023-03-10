4c7b80e011a854d0d8b9935466cfc7bfc0945cb2
computerfolks <104023807+computerfolks@users.noreply.github.com>
2023-03-10 10:42:03 -0500

    Update ReservedWords.java
    I updated and added reserved words that were in the table on the link she put with the assignment

--- src/ReservedWords.java
+++ src/ReservedWords.java
@@ -1,5 +1,9 @@
 public class ReservedWords {
-    static final String[] RESERVED_WORDS = {"program", "var", "begin", "end", "if", "then", "else", "while", "do", "repeat","until", "for", "to", "downto", "write", "writeln"};
+    // taken from https://www.tutorialspoint.com/pascal/pascal_basic_syntax.htm
+    static final String[] RESERVED_WORDS = { "program", "var", "begin", "end", "if", "then", "else", "while", "do",
+            "repeat", "until", "for", "to", "downto", "write", "writeln", "and", "array", "case", "const", "div", "file",
+            "function", "goto", "in", "label", "mod", "nil", "not", "of", "or", "packed", "procedure", "record", "set", 
+            "type", "with", "input", "output", "real", "readline", "writeline", "readln"   };
 
     public static boolean isReservedWord(String s) {
         for (String word: RESERVED_WORDS) {
