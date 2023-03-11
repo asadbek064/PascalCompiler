ebb3bd26d5d64cb6da75ce1f33a83963466ffe57
Asadbek Karimov <asadbekasadbek@gmail.com>
2023-03-11 13:35:53 -0500

    Input txt added

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,6 @@
-
 public class Main {
 
     public static void main(String[] args) {
-        // testing structs
-        String line = "PROGRAM  ChangeMaker".toLowerCase();
-
-        System.out.println(Operators.isValidCharacters(line.charAt(0)));
-        System.out.println(ReservedWords.isReservedWord(line));
-
-        String line2 = "Remainder := 100 - Cost; x and y".toLowerCase();
-
-        System.out.println(ValidCharacters.isArithmeticOperator(line2));
-        System.out.println(ValidCharacters.isRelationalOperator(line2));
-        System.out.println(ValidCharacters.isLogicalOperator(line2));
-
+      
     }
 }
