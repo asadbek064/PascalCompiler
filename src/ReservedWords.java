d44a5d2a1dc4f043001f063c62614931755d2ada
computerfolks <104023807+computerfolks@users.noreply.github.com>
2023-03-11 23:32:11 -0500

    Update ReservedWords.java

--- src/ReservedWords.java
+++ src/ReservedWords.java
@@ -2,17 +2,18 @@ public class ReservedWords {
     // taken from https://www.tutorialspoint.com/pascal/pascal_basic_syntax.htm
     static final String[] RESERVED_WORDS = { "program", "var", "begin", "end", "if", "then", "else", "while", "do",
             "repeat", "until", "for", "to", "downto", "write", "writeln", "array", "case", "const", "div", "file",
-            "function", "goto", "in", "label", "mod", "nil", "of", "packed", "procedure", "record", "set", 
-            "type", "with", "input", "output", "real", "readline", "writeline", "readln"   };
-    //not, or, and are all reserved words but I left them out for now because they are logical operators, not sure how we are handling that yet
+            "function", "goto", "in", "label", "mod", "nil", "of", "packed", "procedure", "record", "set",
+            "type", "with", "input", "output", "real", "readline", "writeline", "readln" };
+    // not, or, and are all reserved words but I left them out for now because they
+    // are logical operators, not sure how we are handling that yet
 
     public static boolean isReservedWord(String s) {
-        for (String word: RESERVED_WORDS) {
-            if(s.contains(word)) {
-                return  true;
+        s = s.toLowerCase().trim();
+        for (String word : RESERVED_WORDS) {
+            if (s.compareTo(word) == 0) {
+                return true;
             }
         }
-        return  false;
+        return false;
     }
 }
-
